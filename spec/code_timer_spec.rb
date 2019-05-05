2e9f9203ed691704dfafebc814be6791f4aee325
David Thai <dafuloth@users.noreply.github.com>
2019-05-05 06:52:27 +0100

    refactor the test

--- spec/code_timer_spec.rb
+++ spec/code_timer_spec.rb
@@ -1,15 +1,14 @@
 require_relative '../lib/code_timer.rb'
 
 describe 'CodeTimer' do
+  array = [297, 182, 332, 302, 112, 92, 224]
+  code_timer = CodeTimer.new(array)
+
   it '.last returns execution time of `Array.last`' do
-    array = [297, 182, 332, 302, 112, 92, 224]
-    code_timer = CodeTimer.new(array)
     expect(code_timer.last).to be_an_instance_of Float
   end
 
   it '.reverse returns execution time of `Array.reverse`' do
-    array = [297, 182, 332, 302, 112, 92, 224]
-    code_timer = CodeTimer.new(array)
     expect(code_timer.reverse).to be_an_instance_of Float
   end
 end
