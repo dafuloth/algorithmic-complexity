2ac677a86ec1fb0dae0193d361ec4451a69a5861
David Thai <dafuloth@users.noreply.github.com>
2019-05-07 02:26:15 +0100

    pushing before trying windows

--- spec/code_timer_spec.rb
+++ spec/code_timer_spec.rb
@@ -4,19 +4,26 @@ describe 'CodeTimer' do
   array = [297, 182, 332, 302, 112, 92, 224]
   code_timer = CodeTimer.new(array)
 
-  it '.last returns execution time of `Array.last`' do
-    expect(code_timer.last).to be_an_instance_of Float
+  describe 'returns the time needed to execute a function' do
+    it '.last returns execution time of `Array.last`' do
+      expect(code_timer.last).to be_an_instance_of Float
+    end
+
+    it '.reverse returns execution time of `Array.reverse`' do
+      expect(code_timer.reverse).to be_an_instance_of Float
+    end
+
+    it '.shuffle returns execution time of `Array.shuffle`' do
+      expect(code_timer.shuffle).to be_an_instance_of Float
+    end
+
+    it '.sort returns execution time of `Array.sort`' do
+      expect(code_timer.sort).to be_an_instance_of Float
+    end
   end
 
-  it '.reverse returns execution time of `Array.reverse`' do
-    expect(code_timer.reverse).to be_an_instance_of Float
-  end
-
-  it '.shuffle returns execution time of `Array.shuffle`' do
-    expect(code_timer.shuffle).to be_an_instance_of Float
-  end
-
-  it '.sort returns execution time of `Array.sort`' do
-    expect(code_timer.sort).to be_an_instance_of Float
+  describe 'Create arrays of different sizes: 5000 to 100000, steps of 5000' do
+    it 'does something' do
+    end
   end
 end
