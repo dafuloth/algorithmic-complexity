7992a3e0361b33f08ae3d0870cb9d29a0356f916
David Thai <dafuloth@users.noreply.github.com>
2019-05-08 13:10:39 +0100

    refactored: duplicates doesn't need to be in a class

--- lib/duplicates.rb
+++ lib/duplicates.rb
@@ -1,14 +1,12 @@
-class AlgDesign
-  def duplicates(input)
-    seen = []
-    duplicates = []
-    input.each do |item|
-      if seen.include?(item)
-        duplicates.push(item) unless duplicates.include?(item)
-      else
-        seen.push(item)
-      end
+def duplicates(input)
+  seen = []
+  duplicates = []
+  input.each do |item|
+    if seen.include?(item)
+      duplicates.push(item) unless duplicates.include?(item)
+    else
+      seen.push(item)
     end
-    duplicates
-    end
+  end
+  duplicates
 end
--- spec/duplicates_spec.rb
+++ spec/duplicates_spec.rb
@@ -13,41 +13,39 @@ require_relative '../lib/duplicates.rb'
 # [3,1,"b",3,"b"]| [3,"b"]
 
 describe 'duplicates test cases' do
-  test = AlgDesign.new
-
   it 'Input: []' do
-    expect(test.duplicates([])).to eq([])
+    expect(duplicates([])).to eq([])
   end
 
   it 'Input: [0]' do
-    expect(test.duplicates([0])).to eq([])
+    expect(duplicates([0])).to eq([])
   end
 
   it 'Input: [0,1]' do
-    expect(test.duplicates([0, 1])).to eq([])
+    expect(duplicates([0, 1])).to eq([])
   end
 
   it 'Input: [1,1]' do
-    expect(test.duplicates([1, 1])).to eq([1])
+    expect(duplicates([1, 1])).to eq([1])
   end
 
   it 'Input: [1,2,3]' do
-    expect(test.duplicates([1, 2, 3])).to eq([])
+    expect(duplicates([1, 2, 3])).to eq([])
   end
 
   it 'Input: [1,2,3,4,2,3]' do
-    expect(test.duplicates([1, 2, 3, 4, 2, 3])).to eq([2, 3])
+    expect(duplicates([1, 2, 3, 4, 2, 3])).to eq([2, 3])
   end
 
   it 'Input: ["one",2,3,2]' do
-    expect(test.duplicates(['one', 2, 3, 2])).to eq([2])
+    expect(duplicates(['one', 2, 3, 2])).to eq([2])
   end
 
   it 'Input: ["two",2,"two"]' do
-    expect(test.duplicates(['two', 2, 'two'])).to eq(['two'])
+    expect(duplicates(['two', 2, 'two'])).to eq(['two'])
   end
 
   it 'Input: [3,1,"b",3,"b"]' do
-    expect(test.duplicates([3, 1, 'b', 3, 'b'])).to eq([3, 'b'])
+    expect(duplicates([3, 1, 'b', 3, 'b'])).to eq([3, 'b'])
   end
 end
